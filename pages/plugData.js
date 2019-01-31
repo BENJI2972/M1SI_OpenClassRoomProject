56ab2e766e37ce3ff0a114ffbbdd9669c724cb8d
Alexandre Mailliu <alex.mailliu21@gmail.com>
2019-01-31 22:53:14 +0100

    homepage v1 done
    - using CDN react for temporary use
    - using plug JSON data for temporary use
    - adding images home-made, css to make it look nicer
    - adding skeleton for other html files

---
+++ pages/plugData.js
@@ -0,0 +1,57 @@
+﻿// fake data to use for development work
+let plugData_lesson1 = {
+  "_id": "47",
+  "title": "Apprenez à programmer en Lorem Ipsum",
+  "durée": "6:66",
+  "niveau": "3",	//(le niveau sera compris entre 1 et 3 -> facile, moyen, difficile)
+  "intro": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit scelerisque congue. Vivamus pharetra tempor urna. Ut dapibus tristique ultricies. Duis ut ipsum congue, rhoncus purus et, luctus urna. Suspendisse fringilla auctor ligula, nec volutpat justo accumsan vitae. Suspendisse potenti. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+  "auteur": "Alexandre Mailliu",
+  "imageURL": "https://i.ytimg.com/vi/BCr7y4SLhck/maxresdefault.jpg",
+  "visible": true,
+  "chapitres": [
+    {
+      "numero" : 1,
+      "sous-parties": [
+        {
+          "numero" : 1,
+          "titre" : "lorem",
+          "contenu" : "Sed quis mi ut velit congue pellentesque vel vitae leo. Nam vitae dictum nulla, in venenatis tellus. Donec rutrum elit nisl, vitae cursus est semper eu. Vivamus in vehicula velit, ut fermentum libero. Aenean tincidunt nibh nulla, quis bibendum risus aliquam a. Fusce posuere elit velit. Suspendisse bibendum massa eu arcu vulputate, ac pretium sapien fermentum. Nulla facilisi. Fusce lectus nibh, cursus et massa quis, sollicitudin viverra augue. Nunc ultrices lectus elit, vitae sollicitudin lacus varius nec. Fusce finibus efficitur auctor. Donec sed elementum odio. Quisque vulputate pharetra lectus nec molestie. Pellentesque vitae ex est. Nulla fermentum leo non dignissim dapibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque orci ante, tincidunt sed suscipit vitae, convallis ac sem. Nam elementum at nisi et feugiat. Nulla varius vel turpis in placerat. Praesent et tincidunt magna, a interdum tortor. Ut egestas purus nibh, sed dapibus sapien sollicitudin non. Nam felis lorem, bibendum eu nisi placerat, congue faucibus erat. Quisque congue a nibh at aliquam. In sem eros, vehicula eu faucibus sed, scelerisque et erat. Donec pretium est vitae dolor varius euismod. Integer consequat efficitur ipsum, id placerat tellus sollicitudin vel. Donec condimentum justo arcu, quis convallis urna consequat sit amet. Duis commodo tortor quis velit varius, in euismod diam consequat. Maecenas semper ipsum sed mauris luctus blandit. Aliquam sapien nunc, laoreet sed ante ut, luctus fermentum urna. Suspendisse eros mi, ullamcorper ut nisl facilisis, rhoncus pulvinar est. Cras tempus pharetra commodo. Sed elit dolor, dignissim id tortor sit amet, mollis aliquam lacus. Maecenas hendrerit, mauris vitae ultricies hendrerit, felis nunc porta metus, convallis consequat purus metus a libero. Phasellus pellentesque a massa sit amet egestas. Nunc viverra eros ac sem vulputate tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce congue nunc vitae purus dictum interdum. Nam elementum elit velit. Maecenas quis sem non velit pretium placerat. Aliquam a tristique orci. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed placerat et nunc eget facilisis. In pharetra commodo arcu vel dignissim. Cras eu viverra eros. Phasellus lobortis lectus vel tortor hendrerit, nec porttitor orci ornare. Praesent fermentum vehicula mi, in dapibus mi vulputate sit amet. Vivamus eget ipsum congue, pharetra tortor vitae, aliquam erat. Cras placerat volutpat placerat. Nunc a vehicula nunc. Mauris tincidunt dapibus scelerisque. Nulla pretium pharetra erat quis semper. Proin tempor pretium placerat. ",
+          "exercice" : [
+            {
+              "numero" : 1,
+              "titre" : "exerciso uno",
+              "reponses": [
+                {
+                  "id": 2,
+                  "valide": true,
+                  "contenu": "Vivamus in vehicula velit, ut fermentum libero."
+                },
+                {
+                  "id": 3,
+                  "valide": false,
+                  "contenu": "Quisque vulputate pharetra lectus nec molestie."
+                }
+              ],
+            },
+            {
+              "numero" : 2 ,
+              "titre" : "exerciso dose",
+              "reponses": [
+                {
+                  "id": 4,
+                  "valide": true,
+                  "contenu": "Nulla fermentum leo non dignissim dapibus."
+                },
+                {
+                  "id": 5,
+                  "valide": false,
+                  "contenu": "Pellentesque orci ante, tincidunt sed suscipit vitae, convallis ac sem."
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
