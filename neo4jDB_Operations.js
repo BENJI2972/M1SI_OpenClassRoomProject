0f4a5a84e1a85f688a9c327c83eb373c792927d2
Romain Kiwi <romainbignon29200@gmail.com>
2019-03-06 18:38:04 +0100

    Pas encore fonctionnelle
    jetez un coup d'oeil, au cas où vous voyez une erreur bête(ou pas lul)

--- neo4jDB_Operations.js
+++ neo4jDB_Operations.js
@@ -17,47 +17,48 @@ var session = null;
 /*const driver = neo4j.driver("bolt://obiwan2.univ-brest.fr:7687");
 const session = driver.session();	*/
 /**
- * this class handles the CRUD basic operations to the mongo database
+ * this class handles the CRUD basic operations to the neo4j database
  */
  
 var Neo4jDB_Operations = (function () {
     function Neo4jDB_Operations(url) {
         this.url = url;
-        
+        console.log(url);
     }
     /**
      * connect to the mongo database.
      * @param functionToDo is the function to call, it has to be a CRUD function
-     * @param jsonQuery is the mongo query, which is always in json format
+     * @param TabParam1 is the mongo query, which is always in json format
      */
-    Neo4jDB_Operations.prototype.connectionNeo4jDB = function (functionToDo, jsonQuery) {
-    //Neo4jDB_Operations.prototype.connectionNeo4jDB = function (functionToDo, jsonQuery, miseAJourReact) {
+    Neo4jDB_Operations.prototype.connectionNeo4jDB = function (functionToDo, TabParam1) {
+    //Neo4jDB_Operations.prototype.connectionNeo4jDB = function (functionToDo, TabParam1, miseAJourReact) {
         session = driver.session();
-		functionToDo(session, jsonQuery, function () {
-                session.close();
+		console.log("Authentification succeed");
+		functionToDo(session, TabParam1, function () {
+				console.log("Disconnection in progress");
+                driver.close();
             }); //miseAJourReact);
     };
     /**
      * read from the mongo database.
      * @param session is the database reference
-     * @param jsonQuery is the mongo query, which is always in json format
+     * @param TabParam1 is the mongo query, which is always in json format
      * @param callback is the callback function, called when this class finished its job
      */
-    //Neo4jDB_Operations.prototype.CreerNoeud = function (session, jsonQuery, callback, miseAJourReact) {
-    Neo4jDB_Operations.prototype.CreerNoeud = function (session, jsonQuery, callback) {
+    //Neo4jDB_Operations.prototype.CreerNoeud = function (session, TabParam1, callback, miseAJourReact) {
+    Neo4jDB_Operations.prototype.CreerNoeud = function (session, TabParam1, callback) {
         console.log("Create")
 		//On a les deux parametres
-		const param0=jsonQuery[0];
-		const param1=jsonQuery[1];
-		console.log("param0:"+ param0);
+		const param0=TabParam1[0];
+		const param1=TabParam1[1];
+		console.log("param1:"+ param1);
 		const resultPromise = session.run(	
-		'CREATE (a:param0 {id:param1 }) RETURN a', //requete pour créer un noeud ayant un nom : param0 et un id: param1 
-		//{param0:param0,param1:param1}
+		'CREATE (a:Noeud {id:$param1}) RETURN a', //requete pour créer un noeud ayant un nom (en dur ici) et un id: param1 
+		{param1:param1}
 	  ); 
 	  callback();
       //miseAJourReact(resultPromise);
     };
- 
     return Neo4jDB_Operations;
 }());
 exports.Neo4jDB_Operations = Neo4jDB_Operations;
@@ -73,5 +74,6 @@ function test1(){
 
 	neo4jDB_Operations.connectionNeo4jDB(neo4jDB_Operations.CreerNoeud, TabParam1);
 }
+
 test1();
 
